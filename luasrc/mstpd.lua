f866bbcde24dc79cdbf21e3ccf2043984bfc8eaa
Anton Kikin <a.kikin@tano-systems.com>
2020-03-11 04:10:14 +0300

    Fix application version display in footer
    Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

--- luasrc/mstpd.lua
+++ luasrc/mstpd.lua
@@ -1,12 +1,12 @@
 --
--- Copyright (c) 2018, Tano Systems. All Rights Reserved.
+-- Copyright (c) 2018-2020, Tano Systems. All Rights Reserved.
 -- Anton Kikin <a.kikin@tano-systems.com>
 --
 
 module("luci.mstpd", package.seeall)
 
-local app_version = "1.2.0"
-local app_home = "https://github.com/tano-systems/luci-app-mstpd"
+local app_version = "1.3.1"
+local app_home = "https://github.com/tano-systems/luci-app-tn-mstpd"
 
 function version()
 	return app_version
